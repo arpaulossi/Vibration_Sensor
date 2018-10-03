92edd7565eb838c3e16630ced97ff4908b23318d
Anderson <anderson.paulossi@bmstecnologia.com.br>
2018-10-03 17:32:05 -0300

    Implementado logica de envio LoRa

--- sketch_teste_kx122/sketch_teste_kx122.ino
+++ sketch_teste_kx122/sketch_teste_kx122.ino
@@ -8,11 +8,12 @@
 #define YOUT_H 0x09
 #define ZOUT_L 0x0A
 #define ZOUT_H 0x0B
-#define MEAS 0x80
+#define R_READ B10000000
 
 #include <SPI.h>
+#include <LoRa.h>
 
-static const int spiClk = 1000000;
+static const int spiClk = 1600000;
 SPIClass * vspi = NULL;
 SPIClass * hspi = NULL;
 
@@ -23,19 +24,31 @@ void initKx122(){
   digitalWrite(5, LOW);
   delay(15);
   vspi->transfer(CNTL1);
-  vspi->transfer(0x40);
+  vspi->transfer(0x60);
+  digitalWrite(5, HIGH);
+  delay(15);
+
+  digitalWrite(5, LOW);
+  vspi->transfer(INC1);
+  vspi->transfer(0x38);
+  digitalWrite(5, HIGH);
+  delay(15);
+
+  digitalWrite(5, LOW);
+  vspi->transfer(INC4);
+  vspi->transfer(0x10);
   digitalWrite(5, HIGH);
   delay(15);
   
   digitalWrite(5, LOW);
   vspi->transfer(ODCNTL);
-  vspi->transfer(0x02);
+  vspi->transfer(0x07);
   digitalWrite(5, HIGH);
   delay(15);
 
   digitalWrite(5, LOW);
   vspi->transfer(CNTL1);
-  vspi->transfer(0xC0);
+  vspi->transfer(0xE0);
   digitalWrite(5, HIGH);
   vspi->endTransaction();
 }
@@ -48,22 +61,22 @@ int16_t readAxis(char axis){
   digitalWrite(5, LOW);
   delay(15);
   if(axis == 'X'){
-    vspi->transfer(XOUT_L | B10000000);
-    outAxis_L = vspi->transfer(MEAS);
-    vspi->transfer(XOUT_H | B10000000);
-    outAxis_H = vspi->transfer(MEAS);
+    vspi->transfer(XOUT_L | R_READ);
+    outAxis_L = vspi->transfer(0x00);
+    vspi->transfer(XOUT_H | R_READ);
+    outAxis_H = vspi->transfer(0x00);
   }
   else if(axis == 'Y'){
-    vspi->transfer(YOUT_L | B10000000);
-    outAxis_L = vspi->transfer(MEAS);
-    vspi->transfer(YOUT_H | B10000000);
-    outAxis_H = vspi->transfer(MEAS);
+    vspi->transfer(YOUT_L | R_READ);
+    outAxis_L = vspi->transfer(0x00);
+    vspi->transfer(YOUT_H | R_READ);
+    outAxis_H = vspi->transfer(0x00);
   }
   else if(axis == 'Z'){
-    vspi->transfer(ZOUT_L | B10000000);
-    outAxis_L = vspi->transfer(MEAS);
-    vspi->transfer(ZOUT_H | B10000000);
-    outAxis_H = vspi->transfer(MEAS);
+    vspi->transfer(ZOUT_L | R_READ);
+    outAxis_L = vspi->transfer(0x00);
+    vspi->transfer(ZOUT_H | R_READ);
+    outAxis_H = vspi->transfer(0x00);
   }
 
   digitalWrite(5, HIGH);
@@ -86,6 +99,7 @@ void setup() {
   pinMode(15, OUTPUT);
 
   Serial.begin(115200);
+  LoRa.begin(915E6);
 
   initKx122();
   Serial.println("Teste de leitura KX122");
@@ -95,6 +109,12 @@ void loop() {
   DataX = readAxis('X');
   DataY = readAxis('Y');
   DataZ = readAxis('Z');
+
+  LoRa.beginPacket();
+  LoRa.print(DataX);
+  LoRa.print(DataY);
+  LoRa.print(DataZ);
+  LoRa.endPacket();
   
   Serial.print("X: ");
   Serial.print(DataX);
